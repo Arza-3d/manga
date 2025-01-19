86137af036db37be479049227176ed91300f527a
Forest Guhareza <arza.3d@gmail.com>
2025-01-19 18:11:10 +0700

    update function.js

--- function.js
+++ function.js
@@ -25,3 +25,23 @@ function generateMangaLink(title, mangaIndex, chapter) {
   return `${baseUrl}${mangaIndexChapter}/${formattedTitle}-chapter-${chapter}`;
 }
 
+function generateHTML(lastVol) {
+  let html = "";
+  let startNum = 1;
+
+  for (let i = 0; i < lastVol.length; i++) {
+    html += `<h3>vol ${i + 1}</h3>\n`;
+    html += "<section>\n";
+
+    for (let j = startNum; j < startNum + lastVol[i]; j++) {
+      html += `${j}\n`;
+    }
+
+    html += "</section>\n\n";
+    startNum += lastVol[i];
+  }
+
+  return html;
+}
+
+const lastVol3 = [3, 12, 15];
