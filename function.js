7f4cef2a0bbabfe156b26e03ce06e16ca80c30a3
Forest Guhareza <arza.3d@gmail.com>
2025-01-19 11:06:43 +0700

    update function.js

--- function.js
+++ function.js
@@ -1,1 +1,19 @@
 
+function parseRange(inputStr) {
+  const result = [];
+
+  inputStr.split(',').forEach((part) => {
+    part = part.trim();
+
+    if (part.includes('-')) {
+      const [start, end] = part.split('-').map(Number);
+      for (let i = start; i <= end; i++) {
+        result.push(i);
+      }
+    } else {
+      result.push(Number(part));
+    }
+  });
+
+  return result;
+}
