b0c7b01a39d1cd259040c4c7936499046f7e2a62
Forest Guhareza <arza.3d@gmail.com>
2025-02-23 18:34:28 +0700

    update naruto.js

--- naruto.js
+++ naruto.js
@@ -34,7 +34,9 @@ const lastChapterInArc = [
   
 const battleLog = [
   ["gaara vs lee", "81-86"]
+  ["naruto vs kiba", "75-77"]
   ["naruto vs neji", "99-104"]
   ["naruto vs sasuke 1st battle"],
+  [""]
   ["shikamaru vs temari", "106-108"]
   ]
