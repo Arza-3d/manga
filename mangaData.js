c50a56012fe6282166569654108f406147cd5e09
Forest Guhareza <arza.3d@gmail.com>
2025-04-07 22:09:31 +0700

    Create mangaData.js

---
+++ mangaData.js
@@ -0,0 +1,28 @@
+let mangaTitle = "My Awesome Manga";
+let mangaChapter = [
+    [1, "The Beginning"],
+    [2, "First Challenge"],
+    // ... add more chapters ...
+    [55, "The Calm Before Storm"],
+    [56, "New Arc Begins"],
+    [99, "Final Countdown"],
+    [100, "Finale Chapter"]
+];
+
+let mangaVolumes = [
+    [1, 1],
+    [11, 2],
+    [21, 3],
+    [31, 4],
+    [41, 5],
+    [51, 6],
+    [61, 7],
+    [71, 8],
+    [81, 9],
+    [91, 10]
+];
+
+let mangaArcs = [
+    [1, "Prologue Arc"],
+    [56, "Final Battle Arc"]
+];
